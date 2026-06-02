161f9fc08db57887086b13281630bca232f3cb62
Daniel Fang <qkfang@hotmail.com>
2026-06-02 13:38:24 +1000

    Remove unnecessary variable assignment in deployment script for cleaner execution

--- bicep/deploy.ps1
+++ bicep/deploy.ps1
@@ -1,6 +1,6 @@
 
 az group create --name 'rg-market-insight' --location 'westus3'
 
-$deployOutput = az deployment group create --name 'mkti-deploy' --resource-group 'rg-market-insight' --template-file './main.bicep' --parameters './main.bicepparam' --query 'properties.outputs' -o json | ConvertFrom-Json
+az deployment group create --name 'mkti-deploy' --resource-group 'rg-market-insight' --template-file './main.bicep' --parameters './main.bicepparam' --query 'properties.outputs' -o json | ConvertFrom-Json
 
 
